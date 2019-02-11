044a08b4e7d08e78e2c61e46d1a95de1bed51466
MariamHmaida <hmaida.mariama@gmail.com>
2019-02-11 11:37:51 +0100

    create a second route

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,24 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class HomeController
 {
+     /**
+     * @Route("/")
+     * */
+    public function homepage()
+    {
+        return new Response("OMG this is my fist page symfony");
+    }
 
+    /**
+     * @Route("/article/mon_article")
+     * */
+    public function read()
+    {
+        return new Response("voici mon article");
+    }
 }
